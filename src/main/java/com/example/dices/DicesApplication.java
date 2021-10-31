09f1e989e0a46486a31918944e4f5f26583884a3
sgoenko <sgoenko@gmail.com>
2021-10-31 13:06:24 +0300

    init

---
+++ src/main/java/com/example/dices/DicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicesApplication.class, args);
+	}
+
+}
